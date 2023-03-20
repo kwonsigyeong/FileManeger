0cc2f88fb8b4061f25bacd4fdc648a1ee25e2310
kwonsigyeong <kwonsigyeong0803@gmail.com>
2023-03-20 11:48:47 +0900

    .

---
+++ src/main/java/com/example/filemaneger/FileManegerApplication.java
@@ -0,0 +1,13 @@
+package com.example.filemaneger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileManegerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileManegerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/filemaneger/FileManegerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.filemaneger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileManegerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
